f702ed59985d63218c49228eab645d42b8218750
thornboo <zhaozhuang1210@gmail.com>
2022-03-10 00:50:44 +0800

    feat: 添加登录表单验证

--- src/view/login/indexL.vue
+++ src/view/login/indexL.vue
@@ -1,24 +1,18 @@
 <template>
   <div class="login-container">
-    <el-form ref="formRef" :model="form" class="login-form">
+    <el-form ref="formRef" :model="form" class="login-form" :rules="rules">
       <div class="title-container">
         <h3 class="title">用户登录</h3>
       </div>
-      <el-form-item>
-        <!--        <el-icon :size="20" class="svg-container">-->
-        <!--          <edit/>-->
-        <!--        </el-icon>-->
+      <el-form-item prop="username">
         <svg-icon icon="user" class="svg-container"></svg-icon>
-        <el-input v-model="form.name"></el-input>
+        <el-input v-model="form.username"></el-input>
       </el-form-item>
-      <el-form-item>
-        <!--        <el-icon :size="20" class="svg-container">-->
-        <!--          <edit/>-->
-        <!--        </el-icon>-->
+      <el-form-item prop="password">
         <svg-icon icon="password" class="svg-container"></svg-icon>
         <el-input v-model="form.password"></el-input>
       </el-form-item>
-      <el-button type="primary" class="login-button">登录</el-button>
+      <el-button type="primary" class="login-button" @click="handleLogin">登录</el-button>
     </el-form>
   </div>
 </template>
@@ -28,9 +22,38 @@ import {ref} from 'vue'
 // import {Edit} from '@element-plus/icons-vue'
 
 const form = ref({
-  name: '',
+  username: '',
   password: ''
 })
+
+const rules = ref({
+  username: [
+    {
+      required: true,
+      message: '请输入用户名',
+      trigger: 'blur',
+    }
+  ],
+  password: [
+    {
+      required: true,
+      message: '请输入密码',
+      trigger: 'blur',
+    }
+  ]
+})
+
+const formRef = ref(null)
+const handleLogin = () => {
+  formRef.value.validate((valid) => {
+    if (valid) {
+     alert('已提交！')
+    } else {
+      console.log('error submit!')
+      return false
+    }
+  })
+}
 </script>
 
 <style lang="scss" scoped>
