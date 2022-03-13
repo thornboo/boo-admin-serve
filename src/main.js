c6b83c2bcd97aeecef55eee0f0f223d9e48f35d5
thornboo <zhaozhuang1210@gmail.com>
2022-03-13 18:31:09 +0800

    feat: 添加路由守卫

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import store from './store'
 import '@/styles/index.scss'
 import SvgIcon from "./icons/index"
 import 'element-plus/dist/index.css'
+import '@/router/permission'
 
 const app = createApp(App)
 SvgIcon(app)
