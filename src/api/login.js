a4db58807f14119aff4892b5a9b9f190a61acb24
thornboo <zhaozhuang1210@gmail.com>
2022-07-06 15:04:43 +0800

    refactor: 对整个项目进行重构

--- src/api/login.js
+++
@@ -1,9 +0,0 @@
-import request from "@/api/request";
-
-export const login = (data) => {
-    return request({
-        url:'/login',
-        method:'POST',
-        data
-    })
-}
--- src/main.js
+++ src/main.js
@@ -1,12 +1,4 @@
-import {createApp} from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
-import '@/styles/index.scss'
-import SvgIcon from "./icons/index"
-import 'element-plus/dist/index.css'
-import '@/router/permission'
 
-const app = createApp(App)
-SvgIcon(app)
-app.use(store).use(router).mount('#app')
+createApp(App).mount('#app')
