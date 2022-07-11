b703f1fe2401b0d227d96161d088de1e5514b0eb
thornboo <zhaozhuang1210@gmail.com>
2022-07-11 20:03:19 +0800

    🔧chore(config): 🐛 fix(config): 优化eslint和prettier的规则

--- .cz-config.js
+++ .cz-config.js
@@ -1,17 +1,15 @@
 module.exports = {
   // 可选类型
   types: [
-    { value: 'feat', name: '⚡ feat: 新功能' },
-    { value: '🐛 fix', name: '🐛 fix: 修复Bug' },
-    { value: 'docs', name: '✏️ docs: 文档变更' },
-    { value: 'style', name: '💄 style: 不影响代码含义的变化(空白、格式化、缺少分号等)' },
-    { value: 'refactor', name: '♻️ refactor: 重构代码，既不修复错误也不添加功能' },
-    { value: 'perf', name: '⚡ perf: 性能优化' },
-    { value: 'test', name: '✅ test: 测试相关' },
-    { value: 'ci', name: '👷 ci: 更改持续集成文件和脚本' },
-    { value: 'chore', name: '📦 chore: 重新打包或更新依赖工具等杂活' },
-    { value: 'revert', name: '⏪ revert: 代码回退' },
-    { value: 'WIP', name: '🚧 WIP: Work in progress' }
+    { value: "✨feat", name: "特性:    一个新的特性" },
+    { value: "🐛fix", name: "修复:    修复一个Bug" },
+    { value: "📝docs", name: "文档:    变更的只有文档" },
+    { value: "💄style", name: "格式:    空格, 分号等格式修复" },
+    { value: "♻refactor", name: "重构:    代码重构，注意和特性、修复区分开" },
+    { value: "⚡perf", name: "性能:    提升性能" },
+    { value: "✅test", name: "测试:    添加一个测试" },
+    { value: "🔧chore", name: "工具:    开发工具变动(构建、脚手架工具等)" },
+    { value: "⏪revert", name: "回滚:    代码回退" },
   ],
   // scope 类型（定义之后，可通过上下键选择）
   scopes: [
@@ -23,7 +21,8 @@ module.exports = {
     ['deps', '项目依赖'],
     ['config', '配置相关'],
     ['other', '其他修改'],
-    // 如果选择 custom，后面会让你再输入一个自定义的 scope。也可以不设置此项，把后面的 allowCustomScopes 设置为 true
+    // 如果选择 custom，后面会让你再输入一个自定义的 scope。
+    // 也可以不设置此项，把后面的 allowCustomScopes 设置为 true
     ['custom', '以上都不是？我要自定义']
   ].map(([value, description]) => {
     return {
@@ -37,8 +36,8 @@ module.exports = {
     type: '确保本次提交遵循规范！\n选择你要提交的类型：',
     scope: '\n选择一个 scope（可选）：',
     customScope: '请输入修改范围（可选）：',
-    subject: '请输入变更描述（必填）：',
-    body: '填写更加详细的变更描述（可选）：',
+    subject: '请输入变更短描述（必填）：',
+    body: '更加详细的变更描述（可选）：',
     footer: '请输入要关闭的 ISSUES （可选）：',
     confirmCommit: '确认提交？'
   },
--- .eslintrc.js
+++ .eslintrc.js
@@ -3,10 +3,13 @@ module.exports = {
   env: {
     node: true
   },
+
   extends: ['plugin:vue/vue3-essential', '@vue/standard'],
+
   parserOptions: {
     parser: '@babel/eslint-parser'
   },
+
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
