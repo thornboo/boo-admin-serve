1caad68954abc719f80559f46538cd2b2bc2baba
thornboo <zhaozhuang1210@gmail.com>
2022-07-12 10:46:28 +0800

    :bug:fix(config): 修复配置文件

--- .cz-config.js
+++ .cz-config.js
@@ -1,15 +1,42 @@
 module.exports = {
   // 可选类型
   types: [
-    { value: "✨feat", name: "特性:    一个新的特性" },
-    { value: "🐛fix", name: "修复:    修复一个Bug" },
-    { value: "📝docs", name: "文档:    变更的只有文档" },
-    { value: "💄style", name: "格式:    空格, 分号等格式修复" },
-    { value: "♻refactor", name: "重构:    代码重构，注意和特性、修复区分开" },
-    { value: "⚡perf", name: "性能:    提升性能" },
-    { value: "✅test", name: "测试:    添加一个测试" },
-    { value: "🔧chore", name: "工具:    开发工具变动(构建、脚手架工具等)" },
-    { value: "⏪revert", name: "回滚:    代码回退" },
+    {
+      value: ':sparkles:feat',
+      name: '特性:    一个新的特性'
+    },
+    {
+      value: ':bug:fix',
+      name: '修复:    修复一个Bug'
+    },
+    {
+      value: ':pencil:docs',
+      name: '文档:    变更的只有文档'
+    },
+    {
+      value: ':lipstick:style',
+      name: '格式:    空格, 分号等格式修复'
+    },
+    {
+      value: ':recycle:refactor',
+      name: '重构:    代码重构，注意和特性、修复区分开'
+    },
+    {
+      value: ':zap:perf',
+      name: '性能:    提升性能'
+    },
+    {
+      value: ':white_check_mark:test',
+      name: '测试:    添加一个测试'
+    },
+    {
+      value: ':hammer:chore',
+      name: '工具:    开发工具变动(构建、脚手架工具等)'
+    },
+    {
+      value: ':rewind:revert',
+      name: '回滚:    代码回退'
+    }
   ],
   // scope 类型（定义之后，可通过上下键选择）
   scopes: [
@@ -33,7 +60,7 @@ module.exports = {
 
   // 步骤消息提示
   messages: {
-    type: '确保本次提交遵循规范！\n选择你要提交的类型：',
+    type: '选择你要提交的类型：',
     scope: '\n选择一个 scope（可选）：',
     customScope: '请输入修改范围（可选）：',
     subject: '请输入变更短描述（必填）：',
