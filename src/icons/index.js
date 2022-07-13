a1778ceda003e8374793257d875c4559a209e432
thornboo <zhaozhuang1210@gmail.com>
2022-07-13 16:58:29 +0800

    refactor: 重新构建vue-ts项目，替换JS

--- src/icons/index.js
+++
@@ -1,8 +0,0 @@
-import SvgIcon from '@/components/SvgIcon'
-
-const svgRequired = require.context('./svg', false, /\.svg$/)
-svgRequired.keys().forEach((item) => svgRequired(item))
-
-export default (app) => {
-  app.component('svg-icon', SvgIcon)
-}
